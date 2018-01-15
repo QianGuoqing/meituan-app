c01fb331e24e3069baa9de42ee9ec420e7bfdc4c
QianGuoqing <qian_guoqing@yeah.net>
2018-01-15 15:51:28 +0800

    完成goods组件主体部分的goods详情列表部分

--- vue-meituan/src/components/goods/goods.vue
+++ vue-meituan/src/components/goods/goods.vue
@@ -20,7 +20,40 @@
     </div>
 
     <!-- 商品列表 -->
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <!-- 专场 -->
+        <li class="container-list">
+          <div v-for="(item, indexx) in container.operation_source_list" :key="indexx">
+            <img :src="item.pic_url" alt="">
+          </div>
+        </li>
+        <!-- 具体分类 -->
+        <li v-for="(item, index) in goods" :key="index" class="food-list">
+          <!-- 具体商品 -->
+          <h3 class="title">{{ item.name }}</h3>
+          <!-- 具体商品列表 -->
+          <ul>
+            <li v-for="(food, index2) in item.spus" :key="index2 + 10000" class="food-item">
+              <div class="icon" :style="head_bg(food.picture)"></div>
+              <div class="content">
+                <h3 class="name">{{ food.name }}</h3>
+                <p class="desc" v-if="food.description">{{ food.description }}</p>
+                <div class="extra">
+                  <span class="saled">{{ food.month_saled_content }}</span>
+                  <span class="praise">{{ food.praise_content }}</span>
+                </div>
+                <img :src="food.product_label_picture" class="product" alt="">
+                <p class="price">
+                  <span class="text">￥{{ food.min_price }}</span>
+                  <span class="unit">/{{ food.unit }}</span>
+                </p>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -45,7 +78,15 @@
       }).catch(error => {
         console.log(error)
       })
-    }
+    },
+    computed: { 
+      
+    },
+    methods: {
+      head_bg(imgName) {
+        return `background-image: url(${imgName})`
+      }
+    },
   }
 </script>
 
