cf6e7a191cc34e2846ee89d72e52030b397200a9
QianGuoqing <qian_guoqing@yeah.net>
2018-01-22 15:23:03 +0800

    完成商品详情页下面的评论模块

--- vue-meituan/src/components/food/food.vue
+++ vue-meituan/src/components/food/food.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="detail">
-    <div class="food" v-show="showFlag">
+    <div class="food" v-show="showFlag" ref="foodView">
       <div class="food-wrapper">
         <div class="food-content">
           <div class="img-wrapper">
@@ -23,6 +23,38 @@
             <div class="buy" v-show="!food.count || food.count == 0" @click="addFirst">选规格</div>
           </div>
         </div>
+        <split></split>
+        <div class="rating-wrapper">
+          <div class="rating-title">
+            <div class="like-ratio" v-if="food.rating">
+              <span class="title">{{ food.rating.title }}</span>
+              <span class="ratio">({{ food.rating.like_ratio_desc }} <i>{{ food.rating.like_ratio }}</i>)</span>
+            </div>
+            <div class="snd-title" v-if="food.rating">
+              <span class="text">{{ food.rating.snd_title }}</span>
+              <span class="icon icon-keyboard_arrow_right"></span>
+            </div>
+          </div>
+          <ul class="rating-content" v-if="food.rating">
+            <li v-for="(comment, index) in food.rating.comment_list" :key="index" class="comment-item">
+              <div class="comment-header">
+                <img :src="comment.user_icon" alt="" v-if="comment.user_icon">
+                <img src="./anonymity.png" alt="" v-if="!comment.user_icon">
+              </div>
+              <div class="comment-main">
+                <div class="user">
+                  {{ comment.user_name }}
+                </div>
+                <div class="time">
+                  {{ comment.comment_time }}
+                </div>
+                <div class="content">
+                  {{ comment.comment_content }}
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </transition>
@@ -31,7 +63,9 @@
 <script>
 
   import Vue from 'vue'
+  import BScroll from 'better-scroll'
   import CartControl from '../cart-control/cart-control.vue'
+  import Split from '../split/split.vue'
 
   export default {
     data() {
@@ -48,6 +82,17 @@
     methods: {
       showView() {
         this.showFlag = true
+
+        // BScroll初始化
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$refs.foodView, {
+              click: true
+            })
+          } else {
+            this.scroll.refresh()
+          }
+        })
       },
       closeView() {
         this.showFlag = false
@@ -57,7 +102,9 @@
       }
     },
     components: {
-      CartControl
+      CartControl,
+      BScroll,
+      Split
     }
   }
 </script>
